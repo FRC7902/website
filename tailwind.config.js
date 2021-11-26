914d45725a3996244c2d324277c5d618399ae165
ArgonBoron23 <argonboron23@gmail.com>
2021-11-26 01:12:36 -0500

    Added palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        firebirds: {
+          blue: {
+            light: {
+              100: "#b2eff0",
+              200: "#65e9ff",
+            },
+            mid: {
+              100: "#4caeff",
+              200: "#0b32b0",
+            },
+            dark: {},
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
