2fea2448ab4197b41ee6562e45a8b24f6e9684c1
ArgonBoron23 <argonboron23@gmail.com>
2021-12-14 01:24:19 -0500

    Implemented header hover rings

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,9 @@ module.exports = {
                     },
                 },
             },
+            ringWidth: {
+                '2.5': '2.5px',
+            }
         },
     },
     variants: {
