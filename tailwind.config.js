4ccd0caa34a7a57b22b7adbd2bebcc23be8a532d
ArgonBoron23 <argonboron23@gmail.com>
2021-12-14 00:43:38 -0500

    Deleted old index, added menu styles, added possible header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        "blue-custom":"#b2efe0",
-        firebirds: {
-          blue: {
-            light: {
-              100: "#b2eff0",
-              200: "#65e9ff",
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "blue-custom": "#b2efe0",
+                firebirds: {
+                    blue: {
+                        light: {
+                            100: "#b2eff0",
+                            500: "#8cecf8",
+                            900: "#65e9ff",
+                        },
+                        mid: {
+                            100: "#4caeff",
+                            200: "#0b32b0",
+                        },
+                        dark: {},
+                    },
+                    red: {
+                        300: "#f35e5e",
+                        400: "#f03737",
+                    },
+                },
             },
-            mid: {
-              100: "#4caeff",
-              200: "#0b32b0",
-            },
-            dark: {},
-          },
-          red: {
-            300: "#f35e5e",
-            400: "#f03737",
-          },
         },
-      },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            ringWidth: ['hover', 'active'],
+            ringColor: ['hover', 'active'],
+        },
+    },
+    plugins: [],
 };
